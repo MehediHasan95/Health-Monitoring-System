3c2c4e113a6be0a51fa9bf602fe4d5aeb9d94569
MehediHasan95 <imehedihasan95@gmail.com>
2022-06-11 21:14:08 +0600

    sum avg

--- health-corner/health-corner.ino
+++ health-corner/health-corner.ino
@@ -1,16 +1,12 @@
 #include <Wire.h>
-
 #include <Adafruit_MLX90614.h>
-
 #include <MAX30100_PulseOximeter.h>
-
 #include <ESP8266WiFi.h>
-
 #include <ESP8266WebServer.h>
 
 #define REPORTING_PERIOD_MS 1000
-#define WIFI_SSID "M18C20"
-#define WIFI_PASS "76187620"
+#define WIFI_SSID "Hasan"
+#define WIFI_PASS "76278704H"
 #define WEB_SERVER_PORT 80
 ESP8266WebServer webServer(WEB_SERVER_PORT);
 uint8_t max30100_address = 0x57;
@@ -18,6 +14,7 @@ uint8_t mlx90614_address = 0x5A;
 uint32_t tsLastReport = 0;
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 PulseOximeter pox;
+
 void setup() {
   Serial.begin(9600);
   setupWiFi();
@@ -30,13 +27,34 @@ void setup() {
 void loop() {
   pox.update();
   if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
-    
+
+    int i;
+    int sensorValue[20];
+    int sum = 0;
+    int avg = 0;
+    int value = pox.getHeartRate();
+
+    for (i = 0; i <= 20; i++) {
+      sensorValue[i] = value;
+    }
+
+    for (i = 0; i <= 20; i++) {
+      sum = sum + sensorValue[i];
+    }
+
+    Serial.print("Total: ");
+    Serial.println(sum);
+    avg = sum / 20;
+    Serial.print("Average: ");
+    Serial.println(avg);
+
+
     Serial.println("Pulse Oximeter:");
     Serial.print(pox.getHeartRate());
     Serial.print(" BPM | SpO2: ");
     Serial.print(pox.getSpO2());
     Serial.println("%");
-    Serial.println("Temperature:");
+    Serial.println("Thermometer:");
     Serial.print(mlx.readObjectTempC());
     Serial.print("°C | ");
     Serial.print(mlx.readObjectTempF());
