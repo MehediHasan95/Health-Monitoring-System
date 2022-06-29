2988ca57bde90bd98d2a66ef462669a4a2c8dbdb
MehediHasan95 <imehedihasan95@gmail.com>
2022-06-29 15:07:49 +0600

    updated

--- Code-1/Code-1.ino
+++ Code-1/Code-1.ino
@@ -4,15 +4,19 @@
 #include <FirebaseArduino.h>
 #include <MAX30100_PulseOximeter.h>
 #include <Wire.h>
-
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 #include "ESP8266WebServer.h"
 #define FIREBASE_HOST "my-project-2a99b-default-rtdb.firebaseio.com"
 #define FIREBASE_AUTH "4kOA0npdTC5R6LreQUMjF6d4mF8dewQ55JJNLjTu"
 #define WIFI_SSID "HASAN"
 #define WIFI_PASSWORD "76278704H"
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
 #define REPORTING_PERIOD_MS 500
 PulseOximeter pox;
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 const int numReadings = 10;
 float filterweight = 0.5;
@@ -54,26 +58,49 @@ void display_calculating(int j) {
   viewBeat();
   Serial.println("Measuring");
   for (int i = 0; i <= j; i++) {
-    Serial.print(".");
+    Serial.print("");
   }
 }
 
 void display_values() {
-  Serial.print("Heart-rate: ");
-  Serial.print(average_beat);
-  Serial.print(" BPM || SpO2: ");
-  Serial.print(average_SpO2);
-  Serial.print("% ");
-  Serial.print("Body-tempC: ");
-  Serial.print(average_tempC);
-  Serial.println();
-
-  //  if (average_beat > 30 and average_SpO2 > 50)
-  //  {
-  //    Firebase.pushFloat("Health-Corner/bpm", average_beat);
-  //    Firebase.pushInt("Health-Corner/spo2", average_SpO2);
-  //  }
+  displayData();
+  StoreDatabase();
 }
+
+void StoreDatabase() {
+  if (average_beat > 60 and average_SpO2 > 70 and average_tempC > 20)
+  {
+    Firebase.pushFloat("Health-Corner/bpm", average_beat);
+    Firebase.pushInt("Health-Corner/spo2", average_SpO2);
+    Firebase.pushFloat("Health-Corner/tempC", average_tempC);
+  }
+}
+
+void displayData()
+{
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+
+
+
+  display.setCursor(0, 10);
+  display.println("BPM: ");
+  display.setCursor(40, 10);
+  display.print(average_beat);
+
+  display.setCursor(0, 20);
+  display.println("SpO2:");
+  display.setCursor(40, 20);
+  display.print(average_SpO2);
+
+  display.setCursor(0, 30);
+  display.println("TempC:");
+  display.setCursor(40, 30);
+  display.print(average_tempC);
+  display.display();
+}
+
 
 void calculate_average(float beat, int SpO2, float tempC) {
   if (readIndex == numReadings) {
@@ -84,7 +111,7 @@ void calculate_average(float beat, int SpO2, float tempC) {
     display_values();
   }
 
-  if (not calculation_complete and beat > 30 and beat < 220 and SpO2 > 50 and tempC > 30) {
+  if (not calculation_complete and beat > 30 and beat < 220 and SpO2 > 50 and tempC > 20) {
     average_beat = filterweight * (beat) + (1 - filterweight) * average_beat;
     average_SpO2 = filterweight * (SpO2) + (1 - filterweight) * average_SpO2;
     average_tempC = filterweight * (tempC) + (1 - filterweight) * average_tempC;
@@ -108,6 +135,11 @@ void firebaseInitialize() {
 void setup() {
   Serial.begin(9600);
   firebaseInitialize();
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+  {
+    Serial.println(F("OLED Display Connection failed"));
+    for (;;);
+  }
   pox.begin();
   mlx.begin();
   setupSensors();
@@ -132,7 +164,6 @@ void loop() {
 }
 
 void setupSensors() {
-  Serial.print("Initializing Pulse Oximeter..");
   if (!pox.begin()) {
     Serial.println("Pulse Oximeter - SETUP FAILED");
     for (;;)
@@ -143,7 +174,6 @@ void setupSensors() {
   }
 
   pox.setIRLedCurrent(MAX30100_LED_CURR_24MA);
-  Serial.println("Initializing MLX Infrared Thermometer..");
   if (!mlx.begin()) {
     Serial.println("MLX Infrared Thermometer - SETUP FAILED");
     for (;;)
